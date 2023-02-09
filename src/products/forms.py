f9280f86ee0786b2e70fdf8c5624f6c6feffb690
jorgesalhani <jorge.salhani@usp.br>
2023-02-09 02:34:43 -0300

    Better understand: Forms, csrf_token

--- src/products/forms.py
+++ src/products/forms.py
@@ -11,3 +11,19 @@ class ProductCreateForm(forms.ModelForm):
             'description',
             'price'
         ]
+
+
+class RawProductForm(forms.Form):
+    title = forms.CharField(label='Title', widget=forms.TextInput(
+        attrs={
+            "placeholder": "Your Title"
+        }
+    ))
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={
+            "class": "new-class-name two",
+            "rows": 10,
+            "cols": 20
+        })
+    )
+    price = forms.DecimalField(max_digits=20, decimal_places=2, initial=199.99)
--- src/products/views.py
+++ src/products/views.py
@@ -1,20 +1,46 @@
 from django.shortcuts import render
 from .models import Product
 from .forms import ProductCreateForm
+from .forms import RawProductForm
 
 # Create your views here.
 
 
 def product_create_view(request):
-    form = ProductCreateForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductCreateForm()
-
+    my_form = RawProductForm()
+    if request.method == 'POST':
+        my_form = RawProductForm(request.POST)
+        if my_form.is_valid():
+            print(my_form.cleaned_data)
+            Product.objects.create(**my_form.cleaned_data)
+        else:
+            print(my_form.errors)
     context = {
-        'form': form
+        'form': my_form
     }
-    return render(request, 'products/product_create.html', context)
+    return render(request, "products/product_create.html", context)
+
+
+# Render with pure FORM
+###
+# def product_create_view(request):
+#     print(request.POST)
+#     context = {}
+#     return render(request, 'products/product_create.html', context)
+
+# Render with Django
+###
+# def product_create_view(request):
+#     form = ProductCreateForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductCreateForm()
+#
+#     context = {
+#         'form': form
+#     }
+#     return render(request, 'products/product_create.html', context)
+
 
 def product_detail_view(request):
     obj = Product.objects.get(id=3)
