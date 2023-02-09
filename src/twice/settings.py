2fb935f5ea2f3f524a0db8edc40efeaf3c51a110
jorgesalhani <jorge.salhani@usp.br>
2023-02-08 22:55:25 -0300

    Page views and html templates

--- src/twice/settings.py
+++ src/twice/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
     # Own apps
     'products',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -67,7 +68,7 @@ ROOT_URLCONF = 'twice.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
