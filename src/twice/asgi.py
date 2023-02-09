046848e9c1c1e50263007b548ca9b49f25cbd4c3
jorgesalhani <jorge.salhani@usp.br>
2023-02-08 21:28:56 -0300

    Twix: Trying twice. No secrets!!

--- src/twice/asgi.py
+++ src/twice/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for firststeps project.
+ASGI config for twice project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firststeps.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twice.settings')
 
 application = get_asgi_application()
